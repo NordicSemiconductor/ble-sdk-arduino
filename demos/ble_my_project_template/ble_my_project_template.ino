dcf87af9bb6931f92c3f26f2dbd88cc89e647e04
Skjerve <Skjerve@users.noreply.github.com>
2014-01-24 16:48:24 +0100

    Update ble_my_project_template.ino

--- demos/ble_my_project_template/ble_my_project_template.ino
+++ demos/ble_my_project_template/ble_my_project_template.ino
@@ -118,7 +118,7 @@ void setup(void)
   Tell the ACI library, the MCU to nRF8001 pin connections.
   The Active pin is optional and can be marked UNUSED
   */	  	
-  aci_state.aci_pins.board_name = REDBEARLAB_V1_1; //See board.h for details
+  aci_state.aci_pins.board_name = REDBEARLAB_SHIELD_V1_1; //See board.h for details
   aci_state.aci_pins.reqn_pin   = 9;
   aci_state.aci_pins.rdyn_pin   = 8;
   aci_state.aci_pins.mosi_pin   = MOSI;
