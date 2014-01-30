abaa33d5a0d5afb154a69eb81884e03ec5d71f33
Skjerve <runar.skjerve@nordicsemi.no>
2014-01-30 15:34:36 +0100

    ARDIN-2-HW-assert-handling ble-modify-setup-data

--- libraries/BLE/examples/ble_modify_setup_data/ble_modify_setup_data.ino
+++ libraries/BLE/examples/ble_modify_setup_data/ble_modify_setup_data.ino
@@ -213,16 +213,16 @@ void setup(void)
   Tell the ACI library, the MCU to nRF8001 pin connections.
   The Active pin is optional and can be marked UNUSED
   */	  	
-  aci_state.aci_pins.board_name = BOARD_DEFAULT; //See board.h for details REDBEARLAB_SHIELD_V1_1
-  aci_state.aci_pins.reqn_pin   = SS;
-  aci_state.aci_pins.rdyn_pin   = 3;
+  aci_state.aci_pins.board_name = REDBEARLAB_SHIELD_V1_1; //See board.h for details REDBEARLAB_SHIELD_V1_1
+  aci_state.aci_pins.reqn_pin   = 9;
+  aci_state.aci_pins.rdyn_pin   = 8;
   aci_state.aci_pins.mosi_pin   = MOSI;
   aci_state.aci_pins.miso_pin   = MISO;
   aci_state.aci_pins.sck_pin    = SCK;
 
   aci_state.aci_pins.spi_clock_divider     = SPI_CLOCK_DIV8;
 	  
-  aci_state.aci_pins.reset_pin             = 4;
+  aci_state.aci_pins.reset_pin             = UNUSED;
   aci_state.aci_pins.active_pin            = UNUSED;
   aci_state.aci_pins.optional_chip_sel_pin = UNUSED;
 	  
