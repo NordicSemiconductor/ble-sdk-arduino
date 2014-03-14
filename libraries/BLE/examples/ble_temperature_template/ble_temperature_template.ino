4cc012b7a65d3784a948254762a2a9b7b62295e7
EAGMnor <emilio.antonio.garcia.morales@nordicsemi.no>
2014-03-14 15:57:56 +0100

    ble_temperature_template done

--- libraries/BLE/examples/ble_temperature_template/ble_temperature_template.ino
+++ libraries/BLE/examples/ble_temperature_template/ble_temperature_template.ino
@@ -39,7 +39,6 @@
  * The format used is IEEE 11073-20601 FLOAT
  */
 #include <SPI.h>
-#include <avr/pgmspace.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
 
@@ -113,6 +112,8 @@ void __ble_assert(const char *file, uint16_t line)
 
 void aci_loop()
 {
+  static bool setup_required = false;
+
   // We enter the if statement only when there is a ACI event available to be processed
   if (lib_aci_event_get(&aci_state, &aci_data))
   {
@@ -133,10 +134,7 @@ void aci_loop()
             */
             aci_state.device_state = ACI_DEVICE_SETUP;
             Serial.println(F("Evt Device Started: Setup"));
-            if (ACI_STATUS_TRANSACTION_COMPLETE != do_aci_setup(&aci_state))
-            {
-              Serial.println(F("Error in ACI Setup"));
-            }
+            setup_required = true;
             break;
 
             case ACI_DEVICE_STANDBY:
@@ -337,6 +335,18 @@ void aci_loop()
     Arduino can go to sleep
     */
   }
+  
+  /* setup_required is set to true when the device starts up and enters setup mode.
+   * It indicates that do_aci_setup() should be called. The flag should be cleared if
+   * do_aci_setup() returns ACI_STATUS_TRANSACTION_COMPLETE.
+   */
+  if(setup_required)
+  {
+    if (SETUP_SUCCESS == do_aci_setup(&aci_state))
+    {
+      setup_required = false;
+    }
+  }
 }
 
 
@@ -384,7 +394,8 @@ void setup(void)
   /** We reset the nRF8001 here by toggling the RESET line connected to the nRF8001
    *  and initialize the data structures required to setup the nRF8001
    */
-  lib_aci_init(&aci_state);
+  //The second parameter is for turning debug printing on for the ACI Commands and Events so they be printed on the Serial
+  lib_aci_init(&aci_state, true);
 
   /**
    * Initalize the data structures required for the GATT Health Thermometer Service
