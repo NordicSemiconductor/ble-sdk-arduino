e318295964c95be5fb3acad3d86cebe687dd2f80
Skjerve <runar.skjerve@nordicsemi.no>
2014-03-21 15:31:54 +0100

    Removed UTF-8 BOM

--- libraries/BLE/dtm.h
+++ libraries/BLE/dtm.h
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2014, Nordic Semiconductor ASA
+/* Copyright (c) 2014, Nordic Semiconductor ASA
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
