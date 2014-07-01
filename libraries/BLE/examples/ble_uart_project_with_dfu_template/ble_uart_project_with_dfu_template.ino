431ba0aaed7506f9554d866a6b80c757f7379105
Mjelva, Morten <morten.mjelva@nordicsemi.no>
2014-07-01 14:00:35 +0200

    Use Redbearlab pin by default

--- libraries/BLE/examples/ble_uart_project_with_dfu_template/ble_uart_project_with_dfu_template.ino
+++ libraries/BLE/examples/ble_uart_project_with_dfu_template/ble_uart_project_with_dfu_template.ino
@@ -157,8 +157,8 @@ void setup(void)
    * The Active pin is optional and can be marked UNUSED
    */
   aci_state.aci_pins.board_name = BOARD_DEFAULT; //See board.h for details REDBEARLAB_SHIELD_V1_1 or BOARD_DEFAULT
-  aci_state.aci_pins.reqn_pin   = SS; //SS for Nordic board, 9 for REDBEARLAB_SHIELD_V1_1
-  aci_state.aci_pins.rdyn_pin   = 3; //3 for Nordic board, 8 for REDBEARLAB_SHIELD_V1_1
+  aci_state.aci_pins.reqn_pin   = 9; //SS for Nordic board, 9 for REDBEARLAB_SHIELD_V1_1
+  aci_state.aci_pins.rdyn_pin   = 8; //3 for Nordic board, 8 for REDBEARLAB_SHIELD_V1_1
   aci_state.aci_pins.mosi_pin   = MOSI;
   aci_state.aci_pins.miso_pin   = MISO;
   aci_state.aci_pins.sck_pin    = SCK;
